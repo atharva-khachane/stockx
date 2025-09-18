d46a61e0f137e243d2035aebf6cd51e22b962c43
atharva-khachane <khachane.ath@gmail.com>
2025-09-19 00:00:59 +0530

    (feat/support): Created the Ticket Part

--- frontend/src/landing_page/support/CreateTicket.js
+++ frontend/src/landing_page/support/CreateTicket.js
@@ -2,7 +2,45 @@
  
  function CreateTicket() {
     return ( 
-        <h1>Create Ticket!!</h1>
+        <div className="container pt-5">
+            <div className="row">
+                <h1 className='fs-4 pt-3'>To create a ticket, select a relevant topic</h1>
+            </div>
+            <div className="row mt-3 mb-5">
+                <div className="col-4   ">
+                    <h4 className='fs-5 pt-3 pb-3'><i className="fa fa-plus-circle" aria-hidden="true"></i> Account Opening</h4>
+                    <ul className="list-unstyled text-small mt-3" style={{fontSize: ".9em"}}>
+                        <li className="pt-2" ><a href="/support/tickets/new" style={{textDecoration:"None"}}>Getting started</a></li>
+                        <li className="pt-2" ><a href="/support/tickets/new" style={{textDecoration:"None"}}>Online</a></li>
+                        <li className="pt-2" ><a href="/support/tickets/new" style={{textDecoration:"None"}}>Offline</a></li>
+                        <li className="pt-2" ><a href="/support/tickets/new" style={{textDecoration:"None"}}>Charges</a></li>
+                        <li className="pt-2" ><a href="/support/tickets/new" style={{textDecoration:"None"}}>Company, Partnership and HUF</a></li>
+                        <li className="pt-2" ><a href="/support/tickets/new" style={{textDecoration:"None"}}>NRI</a></li>
+                    </ul>
+                </div>
+                <div className="col-4   ">
+                    <h4 className='fs-5 pt-3 pb-3'><i className="fa fa-user" aria-hidden="true"></i> Your StockX Account</h4>
+                    <ul className="list-unstyled text-small mt-3" style={{fontSize: ".9em"}}>
+                        <li className="pt-2" ><a href="/support/tickets/new" style={{textDecoration:"None"}}>Login credentials</a></li>
+                        <li className="pt-2" ><a href="/support/tickets/new" style={{textDecoration:"None"}}>Account modification and segment addition</a></li>
+                        <li className="pt-2" ><a href="/support/tickets/new" style={{textDecoration:"None"}}>DP ID and bank details</a></li>
+                        <li className="pt-2" ><a href="/support/tickets/new" style={{textDecoration:"None"}}>Your profile</a></li>
+                        <li className="pt-2" ><a href="/support/tickets/new" style={{textDecoration:"None"}}>Transfer and conversion of shares</a></li>
+                    </ul>
+                </div>
+                <div className="col-4   ">
+                    <h4 className='fs-5 pt-3 pb-3'><i className="fa fa-bar-chart" aria-hidden="true"></i> Trading and Markets</h4>
+                    <ul className="list-unstyled text-small mt-3" style={{fontSize: ".9em"}}>
+                        <li className="pt-2" ><a href="/support/tickets/new" style={{textDecoration:"None"}}>Trading FAQs</a></li>
+                        <li className="pt-2" ><a href="/support/tickets/new" style={{textDecoration:"None"}}>StockX platforms</a></li>
+                        <li className="pt-2" ><a href="/support/tickets/new" style={{textDecoration:"None"}}>Corporate actions</a></li>
+                        <li className="pt-2" ><a href="/support/tickets/new" style={{textDecoration:"None"}}>Margins</a></li>
+                        <li className="pt-2" ><a href="/support/tickets/new" style={{textDecoration:"None"}}>Product and order types</a></li>
+                        <li className="pt-2" ><a href="/support/tickets/new" style={{textDecoration:"None"}}>Taxes</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
      );
  }
  
